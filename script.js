0ef003a5d8e9319b2f28fe505b7ffd5f7b2d7b74
ameenstudio <200854608+ameenstudio@users.noreply.github.com>
2025-03-24 21:24:10 -0600

    final look

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ mainEl.classList.add('flex-ctr');
 let topMenuEl = document.querySelector('#top-menu');
 topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = "var(--top-menu-bg)"
-topMenuEl.style.justifyContent = 'space-around';
+topMenuEl.classList.add('flex-around');
 
 
 
