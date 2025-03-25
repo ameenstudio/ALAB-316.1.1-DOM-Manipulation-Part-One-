5f3cd60d29edf3baca416061296538d80e71567a
ameenstudio <200854608+ameenstudio@users.noreply.github.com>
2025-03-24 21:02:17 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+];
+let nav=document.getElementById('top-menu');
+
+for (let i = 0; i < menuLinks.length; i++) {
+
+    let navMenu=document.createElement('div')
+    navMenu.textContent= menuLinks[i].text
+    navMenu.classList.add('navMenu')
+    nav.appendChild('navMenu')
+
+}
+
+
+
+
+// console.log(menuLinks);
+// let nav = menuLinks[i]
+nav = document.getElementById('top-menu');
+let navMenu = document.createElement('nav')
+
+
+
+
+
+let mainEl = document.querySelector('main');//Select and cache the <main> element in a variable named mainEl.
+mainEl.style.backgroundColor = 'var(--main-bg)'
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>'
+mainEl.classList.add('flex-ctr');
+let topMenuEl = document.querySelector('#top-menu');
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)"
+topMenuEl.style.flexAround
+
+
+
+
+
+
+
+// console.log(mainEl)
